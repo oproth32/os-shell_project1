24f1cef05adcdac1f15f26d77975303e6a080384
Owen Proth <lionking3223@icloud.com>
2025-09-22 15:07:36 -0400

    Same as last commit, forgot to save changes

--- src/lexer.c
+++ src/lexer.c
@@ -1,35 +1,9 @@
 #include "lexer.h"
-#include "prompt.h"
 #include "environmentVariables.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main()
-{
-	while (1) {
-		// print the username, machine name and current directory like in a normal shell
-		// e.g. user@machine:/current/directory
-		prompt();
-
-		/* input contains the whole command
-		 * tokens contains substrings from input split by spaces
-		 */
-
-		char *input = get_input();
-		printf("whole input: %s\n", input);
-
-		tokenlist *tokens = get_tokens(input);
-		for (int i = 0; i < tokens->size; i++) {
-			printf("token %d: (%s)\n", i, tokens->items[i]);
-		}
-
-		free(input);
-		free_tokens(tokens);
-	}
-
-	return 0;
-}
 
 char *get_input(void) {
 	char *buffer = NULL;
